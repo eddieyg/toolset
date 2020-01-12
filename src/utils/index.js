8cc7c9913feb91c6c725cc215a0c286884ba5a16
eddie <42637988+eddieyg@users.noreply.github.com>
2020-01-12 16:51:01 +0800

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -25,5 +25,51 @@ export default {
   createRandomNum () {
     let randomNum = Math.round(Math.random() * 99999)
     return (+new Date()) + '' + randomNum
+  },
+  
+  /**
+   * 获取时间戳距离今天还有多少天
+   * （不按具体的24小时一天算，按天差算。如现在22点，距离时间戳还有3小时，但依然按1天距离算）
+   **/
+
+  getSurplusDays (endTime) {
+    if (!parseInt(endTime)) console.error('endTime时间戳参数有误')
+    endTime = parseInt(endTime)
+    nowDate = new Date()
+    const result = {}
+    const dayMillisecond = 1000 * 60 * 60 * 24
+    let timeGap = endTime - +nowDate
+    let isPast = false
+    let dayGap
+
+    // 时间戳已过去
+    if (timeGap < 0) {
+      isPast = true
+      timeGap = String(timeGap).replace('-', '') * 1
+    }
+
+    dayGap = timeGap / dayMillisecond
+    
+    // 天数间隙是否为整数
+    if (dayGap % 1 === 0) {
+      result.days = dayGap
+    } else {
+      let decimal = String(dayGap).replace(/^\d+\./, '0.') * 1
+      let todayGap
+      if (isPast) {
+        todayGap = +nowDate - +new Date(nowDate.toDateString() + ' 00:00:00')
+      } else {
+        todayGap = +new Date(nowDate.toDateString() + ' 23:59:59') - +nowDate
+      }
+      todayGap = todayGap / dayMillisecond
+      result.days = decimal - todayGap <= 0 ? Math.floor(dayGap) : Math.ceil(dayGap)
+      console.log(dayGap)
+      console.log(decimal)
+      console.log(todayGap)
+    }
+
+    result.type = isPast ? 'past' : 'future'
+    return result
   }
+  
 }
